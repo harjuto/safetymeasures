900be9bed33425f302e09d1d759172e60ee13f66
Tomi Harju <thar@fl815.local>
2016-03-08 19:54:12 +0200

    Moving spa under nginx

--- server/index.js
+++ server/index.js
@@ -11,17 +11,6 @@ var path = require('path');
 
 app.use(cors());
 // gzip
-app.use(compression());
-//Allow S3 signing
 app.get('/sign_s3', require('./src/amazon'));
-
-var root = path.resolve(__dirname + '/../build/');
-app.use(express.static(root));
-
-app.use('*', function(req, res) {
-  res.redirect('/');
-})
-
-
 
 server.listen(1337);
